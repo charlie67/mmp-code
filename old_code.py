9d6ab76099efc384e38d7461eb44e69b15e2990b
Charlie <charlierobinson080@gmail.com>
2020-02-15 15:43:32 +0000

    Old DBSCAN code

---
+++ old_code.py
@@ -0,0 +1,30 @@
+# DBSCAN clustering
+db = DBSCAN(eps=3, min_samples=3).fit(X)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+db_labels = db.labels_
+db_n_clusters_ = len(set(db_labels)) - (1 if -1 in db_labels else 0)
+n_noise_ = list(db_labels).count(-1)
+
+# came from https://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
+# Black removed and is used for noise instead.
+unique_labels = set(db_labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (db_labels == k)
+
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=14)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % db_n_clusters_)
+plt.show()
