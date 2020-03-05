1c2896b02ee2205038c9a6e74d3fb74541b5d5b0
Charlie <charlierobinson080@gmail.com>
2020-03-05 03:27:39 +0000

    mmp-25 start  looking at shortest path within clusters

--- Main.py
+++ Main.py
@@ -102,6 +102,27 @@ def plot_tour(tour, clustered_data: ClusteredData):
     plt.show()
 
 
+def calculate_shortest_paths_for_cluster_using_aco(cluster: Cluster):
+    solver = acopy.Solver(rho=.03, q=1)
+    colony = acopy.Colony(alpha=1, beta=10)
+
+    # printout_plugin = acopy.plugins.Printout()
+    # solver.add_plugin(printout_plugin)
+
+    graph = cluster.turn_cluster_into_networkx_graph()
+    cluster_tour = solver.solve(graph, colony, limit=300)
+
+    print(cluster_tour)
+
+
+def perform_aco_over_clustered_problem():
+    solver = acopy.Solver(rho=.03, q=1)
+    colony = acopy.Colony(alpha=1, beta=10)
+    printout_plugin = acopy.plugins.Printout()
+    solver.add_plugin(printout_plugin)
+    return solver.solve(graph, colony, limit=300)
+
+
 if __name__ == '__main__':
     directory_name = "testdata/world/"
 
@@ -144,15 +165,11 @@ if __name__ == '__main__':
 
         graph = optics_clustered_data.turn_clusters_into_nx_graph(tsplib_problem=problem)
 
-        solver = acopy.Solver(rho=.03, q=1)
-        colony = acopy.Colony(alpha=1, beta=10)
-
-        printout_plugin = acopy.plugins.Printout()
-        solver.add_plugin(printout_plugin)
-
-        tour = solver.solve(graph, colony, limit=300)
+        tour = perform_aco_over_clustered_problem()
         plot_tour(tour.nodes, optics_clustered_data)
 
         find_movement_between_clusters(tour.nodes, optics_clustered_data, problem_data_array)
+
+        calculate_shortest_paths_for_cluster_using_aco(cluster=optics_clustered_data.clusters[0])
 
         print(tour.nodes)
