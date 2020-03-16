24098b6fda0baef4ed76c3122868d61e778265c3
Charlie <charlierobinson080@gmail.com>
2020-03-16 10:04:09 +0000

    Fix Jenkins tests not running

--- tests/load_tsp_file_test.py
+++ tests/load_tsp_file_test.py
@@ -5,6 +5,6 @@ from Loading import load_problem_into_np_array
 
 class TestClusteredData(TestCase):
     def test_cluster_neighbour_movement(self):
-        problem, problem_data_array = load_problem_into_np_array("../testdata/world/dj38.tsp")
+        problem, problem_data_array = load_problem_into_np_array("testdata/world/dj38.tsp")
 
         self.assertCountEqual(problem_data_array.shape, (38, 2), "The shape of the numpy array is incorrect")
